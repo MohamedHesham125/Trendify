0d6371ff9e450929f19bb1d1080053302ee36598
ssaammiissaammii <samikasem1973@gmail.com>
2024-09-26 15:57:29 +0300

    Add Settinge Screen

---
+++ app/src/main/java/com/example/trendify/presentation/ui/SettingScreen.kt
@@ -0,0 +1,13 @@
+package com.example.trendify.presentation.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class SettingScreen:Screen {
+    @Composable
+    override fun Content() {
+
+        Text(text ="sami" )
+    }
+}
