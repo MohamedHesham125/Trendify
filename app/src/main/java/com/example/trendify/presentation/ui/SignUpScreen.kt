caa454b4b408f322ca4137b5e27c6e67d4420f61
Mohammed Hesham <darkkhaled6@gmail.com>
2024-09-27 21:15:34 +0300

    adding signup screen

--- app/src/main/java/com/example/trendify/presentation/ui/SignUpScreen.kt
+++ app/src/main/java/com/example/trendify/presentation/ui/SignUpScreen.kt
@@ -1,26 +1,17 @@
 package com.example.trendify.presentation.ui
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
@@ -30,7 +21,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
@@ -40,15 +30,14 @@ import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import com.example.trendify.R
 
-class LoginSceen : Screen {
+class SignUpScreen : Screen {
     @Composable
     override fun Content() {
-        loginscreen()
+        signupscreen()
     }
 
-
     @Composable
-    fun loginscreen(){
+    fun signupscreen(){
         val username= remember { mutableStateOf("") }
         val password= remember { mutableStateOf("") }
         val navigator= LocalNavigator.currentOrThrow
@@ -58,7 +47,9 @@ class LoginSceen : Screen {
                 text = "Sign In",
                 fontSize = 20.sp,
                 fontFamily = FontFamily(Font(resId = R.font.poppins_bold)),
-                modifier = Modifier.width(73.dp).padding(bottom = 30.dp)
+                modifier = Modifier
+                    .width(73.dp)
+                    .padding(bottom = 30.dp)
             )
         }
         Column(verticalArrangement = Arrangement.Center,
@@ -68,37 +59,41 @@ class LoginSceen : Screen {
             Spacer(modifier = Modifier.height(30.dp))
 
             //Email Address TextField
-                OutlinedTextField(
-                    value =username.value ,
-                    onValueChange ={username.value=it},
-                    modifier = Modifier
-                        .width(343.dp)
-                        .height(64.dp),
-                    colors = OutlinedTextFieldDefaults.colors(
-                        focusedBorderColor = Color(0xff6055D8),
-                        unfocusedBorderColor = Color.White,
-                        cursorColor = Color.Black
-                    ),
-                    label = { Text(text = "Name",
-                        color = Color(0xff9B9B9B))}
+            OutlinedTextField(
+                value =username.value ,
+                onValueChange ={username.value=it},
+                modifier = Modifier
+                    .width(343.dp)
+                    .height(64.dp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color(0xff6055D8),
+                    unfocusedBorderColor = Color.White,
+                    cursorColor = Color.Black
+                ),
+                label = { Text(text = "Name",
+                    color = Color(0xff9B9B9B)
                 )
+                }
+            )
             Spacer(modifier = Modifier.height(30.dp))
 
             //Password TextField
-                OutlinedTextField(
-                    value =password.value ,
-                    onValueChange ={password.value=it},
-                    modifier = Modifier
-                        .width(343.dp)
-                        .height(64.dp),
-                    colors = OutlinedTextFieldDefaults.colors(
-                        focusedBorderColor = Color(0xff6055D8),
-                        unfocusedBorderColor = Color.White,
-                        cursorColor = Color.Black
-                    ),
-                    label = { Text(text = "Password",
-                        color = Color(0xff9B9B9B))}
+            OutlinedTextField(
+                value =password.value ,
+                onValueChange ={password.value=it},
+                modifier = Modifier
+                    .width(343.dp)
+                    .height(64.dp),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color(0xff6055D8),
+                    unfocusedBorderColor = Color.White,
+                    cursorColor = Color.Black
+                ),
+                label = { Text(text = "Password",
+                    color = Color(0xff9B9B9B)
                 )
+                }
+            )
 
 
             Spacer(modifier = Modifier.height(30.dp))
@@ -140,7 +135,7 @@ class LoginSceen : Screen {
                 Text(text = "Signup Free",
                     modifier = Modifier
                         .padding(bottom = 10.dp)
-                        .clickable { navigator.push(SignUpScreen()) })
+                        .clickable { /*TODO*/ })
             }
         }
     }
